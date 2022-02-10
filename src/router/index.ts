db4f109a98dac940445298186073bf41b2b08665
Max <bazarsadtsogtjargal@gmail.com>
2022-02-10 21:29:18 +0900

    initial setup register and login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,22 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+import Home from '../views/Home.vue';
+import Layout from '../views/Layout.vue';
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+  { path: '/login', component: Login },
+  { path: '/register', component: Register },
+  { path: '', component: Layout },
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
